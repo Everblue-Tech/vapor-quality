8c949bacbcf7b8d550bca263f4c1a354bb5261db
Emily Armstrong <emilythearmstrong@gmail.com>
2025-05-15 17:18:38 -0400

    update shell script to inject additional env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 echo "Substituting env vars into nginx.conf"
+echo "REACT_APP_VAPORFLOW_NO_PROTOCOL = ${REACT_APP_VAPORFLOW_NO_PROTOCOL}"
 echo "REACT_APP_VAPORFLOW_URL = ${REACT_APP_VAPORFLOW_URL}"
 
-envsubst '${REACT_APP_VAPORFLOW_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${REACT_APP_VAPORFLOW_NO_PROTOCOL} ${REACT_APP_VAPORFLOW_URL}' \
+  < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-cat /etc/nginx/nginx.conf 
+cat /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
