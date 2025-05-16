2e79f147efd5d41fc20e596ab60b208b6430623c
Emily Armstrong <emilythearmstrong@gmail.com>
2025-05-16 12:33:57 -0400

    revert change to build.js

--- scripts/build.js
+++ scripts/build.js
@@ -225,6 +225,6 @@ async function build(previousFileSizes) {
 function copyPublicFolder() {
   fs.copySync(paths.appPublic, paths.appBuild, {
     dereference: true,
-    filter: file => true,
+    filter: file => file !== paths.appHtml,
   });
 }
